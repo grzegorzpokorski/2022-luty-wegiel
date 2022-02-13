18689f257b193e579c87b8522eed36932b6b49ca
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-02-13 16:08:15 +0100

    added hero-slider

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,27 @@ import 'bootstrap';
 import { tns } from "./../../node_modules/tiny-slider/src/tiny-slider"
 
 var slider = tns({
-	container: '#testimonials-slider',
-	controlsContainer: "#customize-controls",
+	container: '.testimonials-slider',
+	controlsContainer: ".testimonials-slider__controls",
 	mode: 'carousel',
 	items: 1,
 	slideBy: 'page',
 	navPosition: 'bottom',
 	mouseDrag: true,
 	autoHeight: true,
+});
+
+var slider = tns({
+	container: '.hero-slider',
+	controlsContainer: ".hero-slider__controls",
+	mode: 'carousel',
+	items: 1,
+	slideBy: 'page',
+	navPosition: 'bottom',
+	mouseDrag: true,
+	autoHeight: true,
+	autoplay: true,
+	autoplayHoverPause: true,
+	autoplayButtonOutput: false,
+	speed: 700,
 });
