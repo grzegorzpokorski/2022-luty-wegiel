38fd5dbc44bab28a896e3e2a26c96be5585d3471
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-02-13 12:07:03 +0100

    added testimonials slider based on tiny-slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 import 'bootstrap';
+import { tns } from "./../../node_modules/tiny-slider/src/tiny-slider"
+
+var slider = tns({
+	container: '#testimonials-slider',
+	controlsContainer: "#customize-controls",
+	mode: 'carousel',
+	items: 1,
+	slideBy: 'page',
+	navPosition: 'bottom',
+	prevButton: '.prev',
+});
